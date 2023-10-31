11e89d8e782683a6c9466392e11d82ed7a058d41
Edu-RVN <evidalnegreiros@gmail.com>
2023-10-31 13:52:05 -0300

    correção de erros javaScript

--- WebWal/Scripts/MyScripts/AdmTratamento.js
+++ WebWal/Scripts/MyScripts/AdmTratamento.js
@@ -1,12 +1,37 @@
 ﻿$(document).ready(function () {
-
+    debugger
     const urlAtual = window.location.pathname;
 
     if (urlAtual === "/Home/CustomerAppointment") {
-
+        getClient()
        
     }
 
 
 
 });
+
+
+function getClient() {
+    $("#btnGetCliente").on("click", function () {
+        $("#frmTratamento").css("display", "block");
+        exibirRelatorio();
+    });
+}
+
+function exibirRelatorio() {
+    $("#frmTratamento").on("change", function () {
+        $("#frmRelatorios").css("display", "block");
+        exibirBotaoEnviar();
+    })
+}
+
+function exibirBotaoEnviar() {
+    $("#txtValor").focusout(function () {
+        var valor = $("#txtValor").val();
+        
+        if (valor !== "") {
+            $("#btnEnviarTratamento").css("display", "block");;
+        }
+    });
+}
